7f04eee0f8d8aa9388b0ce188e5a33303526b370
BetterTomorrowPy <13540471106@163.com>
2017-06-06 16:13:56 +0800

    add test

--- luxury/apis/models.py
+++ luxury/apis/models.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+    django test.
+"""
+
 from django.db import models
 
-# Create your models here.
+
+class Animal(models.Model):
+    """"""
+    name = models.CharField('名字', max_length=64)
+    sound = models.CharField(max_length=64, blank=True, default='')
+
+    def speak(self):
+        return self.sound
--- luxury/apis/tests.py
+++ luxury/apis/tests.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+    test.
+"""
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Animal
+
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Animal.objects.create(name='lion', sound='roar')
+        Animal.objects.create(name='cat', sound='meow')
+
+    def test_animals_can_speak(self):
+        lino = Animal.objects.get(name='lino')
+        cat = Animal.objects.get(name='cat')
+        self.assertEqual(lino.speak(), 'roar')
+        self.assertEqual(cat.speak(), 'meow')
